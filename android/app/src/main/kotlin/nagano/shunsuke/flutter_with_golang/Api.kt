36de607b5591718af262b17247c728600bdf0401
永野峻輔 <peace.infi0407@gmail.com>
2021-04-23 13:53:25 +0900

    android設定

---
+++ android/app/src/main/kotlin/nagano/shunsuke/flutter_with_golang/Api.kt
@@ -0,0 +1,26 @@
+package nagano.shunsuke.flutter_with_golang
+
+
+import api.Api
+import nagano.shunsuke.plugins.Pigeon
+
+class Api() : Pigeon.Api {
+    override fun call(): Pigeon.Response {
+        val response = Pigeon.Response()
+        response.message = Api.goCall()
+        return response
+    }
+
+    override fun eval(arg: Pigeon.Request): Pigeon.Response {
+        val response = Pigeon.Response()
+        return try {
+            val result = Api.evaluate(arg.expression)
+            response.message = result.toString()
+            response
+        } catch (e: Exception ) {
+            response.message = e.toString()
+            response
+        }
+
+    }
+}
--- android/app/src/main/kotlin/nagano/shunsuke/flutter_with_golang/MainActivity.kt
+++ android/app/src/main/kotlin/nagano/shunsuke/flutter_with_golang/MainActivity.kt
@@ -1,6 +1,14 @@
 package nagano.shunsuke.flutter_with_golang
 
+import androidx.annotation.NonNull
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import nagano.shunsuke.plugins.Pigeon
+
 
 class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        Pigeon.Api.setup(flutterEngine.dartExecutor.binaryMessenger,  Api())
+    }
 }
